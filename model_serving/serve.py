36a8a02b193ac2caa587356413829a2018bd24b0
Praveen Kumar Thopalle <praveen.thopalle@gmail.com>
2025-02-26 21:15:38 -0800

    Serve module changes to easy switch to base model as well

--- model_serving/serve.py
+++ model_serving/serve.py
@@ -15,6 +15,7 @@ serve.start()
 class ModelDeployment:
     def __init__(self):
         self.model = SimpleModel()
+        #replace your own trained_model path or download base model from hugging face
         self.model.load_state_dict(torch.load("trained_model.pth"))
         self.model.eval()
 
